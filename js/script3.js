18b114c398ab1768ce546507ed2e7dbfd0118f88
Cauã Lucas <126674336+cauablindado11@users.noreply.github.com>
2023-06-30 13:59:15 -0300

    commit

---
+++ js/script3.js
@@ -0,0 +1,11 @@
+//contador
+
+let numero = parseInt(prompt("Digite um valor: "));
+let contagem = "Contagem: ";
+
+for (let i = 1; i <= numero; i++) {
+    contagem += i + " ";
+}
+
+contagem += "Finalizou!!!";
+console.log(contagem);
